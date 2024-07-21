afd9d1d81bf07d73ef67f573b96c9b5deef4256d
MamadouPNDIAYE <134697046+MamadouPNDIAYE@users.noreply.github.com>
2024-07-21 01:09:40 +0000

    Update iris.py

--- iris.py
+++ iris.py
@@ -1,38 +1,43 @@
 import streamlit as st
+import pandas as pd
 from sklearn import datasets 
 from sklearn.ensemble import RandomForestClassifier
-import pandas as pd
 
-# Chargement du dataset Iris et Random Forest Classifier
-iris = datasets.load_iris()
-x = iris.data
-y = iris.target
-foret=RandomForestClassifier()  
-foret.fit(x,y)
+st.write('''
+# App pour la prevision des fleurs
+cette application predit la catégorie des fleurs.
+''')
+st.image("plante.jfif",width=500)
+st.sidebar.header("les parametres d'entrée") 
 
-# Creation de l'application de prediction titree et en tete
-st.title("Application de prévision des fleurs d'iris")
-st.header("Cette application predit la catégorie des fleurs d'iris")
+#===============================================================================================================
 
-# saisir les longueurs et largeurs de sepal et petal à laide de la fonction Slider() de streamlit
-st.sidebar.header("Les parametres d'entrée des fleurs d'iris")
-sepal_length = st.sidebar.slider('Sepal length',4.3,7.9,5.3)
-sepal_width = st.sidebar.slider('Sepal width',2.0,4.4,3.3)
-petal_length = st.sidebar.slider('petal length',1.0,6.9,2.3)
-petal_width = st.sidebar.slider('petal width',0.1,2.5,1.3)
-st.sidebar.write('This App is creat by MP_NDIAYE')
-
-data = {'sepal_length':sepal_length,
+def user_input():
+    sepal_length = st.sidebar.slider('Entrer la longueur du sepal:',4.3,10.0,0.0)
+    sepal_width = st.sidebar.slider('Entrer la largeur du sepal:',2.0,10.0,0.0)
+    petal_length = st.sidebar.slider('Entrer la longueur du petal:',1.0,10.0,0.0)
+    petal_width = st.sidebar.slider('Entrer la largeur du petal:',0.1,10.0,0.0)
+    data = {'sepal_length':sepal_length,
             'sepal_width':sepal_width,
             'petal_length':petal_length,
             'petal_width':petal_width}
-df = pd.DataFrame(data,index=[0])
+    fleur_parametre = pd.DataFrame(data,index=[0])
+    return fleur_parametre
 
-# Affichage des parametres saisi par l'utilisateur dans un subheader cad paragraphe
+#===============================================================================================================
+
 st.subheader('On veut trouver la catégorie de cette fleur')
+df = user_input()
 st.write(df)
 
-# Definition du boutton de prediction et afichage du valeur de fleur predit
-if(st.button("Submit")):
+iris = datasets.load_iris()
+foret=RandomForestClassifier()  
+foret.fit(iris.data, iris.target)  
+
+#===============================================================================================================
+
+if(st.button("▶️")):
     prediction = foret.predict(df)
-    st.write("la catégorie de la fleur d'iris est:",iris.target_names[prediction])
+    st.write("La catégorie de la fleur est:")
+    st.success(iris.target_names[prediction])
+    
